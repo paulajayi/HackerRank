1dca9e853969cfb1f757131784c1d0a90ca1a9e0
Paul Ajayi <paul.ajayi@elevationng.org>
2021-06-28 17:28:24 +0100

    update at 26/06/2021"

---
+++ Algorithm/c#/HackerRank/CompareTheTriplets/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            List<int> result = compareTriplets(a, b);
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+
+
+
+        static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+
+            int aResult = 0;
+            int bResult = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    aResult++;
+                }
+                else if (a[i] < b[i])
+                {
+                    bResult++;
+                }
+
+
+            }
+
+            var arr = new int[] { aResult, bResult };
+            List<int> arr_list = arr.OfType<int>().ToList();
+            return arr_list;
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Algorithm/c#/HackerRank/SimpleArraySum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int arCount = Convert.ToInt32(Console.ReadLine());
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = simpleArraySum(ar);
+
+         
+            Console.WriteLine(result);
+
+        }
+
+
+
+        static int simpleArraySum(int[] ar)
+        {
+           
+            int sum = 0;
+            foreach (var item in ar)
+            {
+                sum += item;
+            }
+            return sum;
+
+        }
+    }
+}
